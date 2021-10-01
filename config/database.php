ad97d07a5891e01a6054193d542248cee628d366
Muhammad Tubagus Sananto <tubagussananto2000@gmail.com>
2021-10-01 12:21:42 +0700

    fix vendor bca and tranglo rate

--- config/database.php
+++ config/database.php
@@ -56,6 +56,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
+            'timezone'  => '+00:00',
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
